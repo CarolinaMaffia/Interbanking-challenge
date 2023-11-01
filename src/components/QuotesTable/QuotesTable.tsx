ad15243022c298f1f870512c3bfbe19727e2c3b5
Caru <carolinamaffia.m@gmail.com>
2023-11-01 11:26:44 -0300

    Showing mock array in QuotesTable

--- src/components/QuotesTable/QuotesTable.tsx
+++ src/components/QuotesTable/QuotesTable.tsx
@@ -1,38 +1,44 @@
+import QuoteCard from '../QuoteCard/QuoteCard';
 import './QuotesTable.scss';
 
 const QuotesTable = () => {
+    const quotesArray = [
+        {
+            quote: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit autem saepe",
+            author: 'Lorem ipsum'
+        },
+        {
+            quote: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit autem saepe",
+            author: 'Lorem ipsum'
+        },
+        {
+            quote: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit autem saepe",
+            author: 'Lorem ipsum'
+        },
+        {
+            quote: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit autem saepe",
+            author: 'Lorem ipsum'
+        },
+        {
+            quote: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit autem saepe",
+            author: 'Lorem ipsum'
+        },
+        {
+            quote: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit autem saepe",
+            author: 'Lorem ipsum'
+        },
+    ]
     return (
-        <div className="quotestable-container">
-            <div className='fake-card'>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur voluptate doloremque facilis, quo delectus omnis officia e</p>
-            </div>
-            <div className='fake-card'>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur voluptate doloremque facilis, quo delectus omnis officia e</p>
-            </div>
-            <div className='fake-card'>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur voluptate doloremque facilis, quo delectus omnis officia e</p>
-            </div>
-            <div className='fake-card'>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur voluptate doloremque facilis, quo delectus omnis officia e</p>
-            </div>
-            <div className='fake-card'>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur voluptate doloremque facilis, quo delectus omnis officia e</p>
-            </div>
-            <div className='fake-card'>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur voluptate doloremque facilis, quo delectus omnis officia e</p>
-            </div>
-            <div className='fake-card'>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur voluptate doloremque facilis, quo delectus omnis officia e</p>
-            </div>
-            <div className='fake-card'>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur voluptate doloremque facilis, quo delectus omnis officia e</p>
-            </div>
-            <div className='fake-card'>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur voluptate doloremque facilis, quo delectus omnis officia e</p>
-            </div>
-            <div className='fake-card'>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur voluptate doloremque facilis, quo delectus omnis officia e</p>
-            </div>
+        <div className="quotes-table-container">
+           {quotesArray.map((quote, i) => {
+            return (
+                <QuoteCard 
+                    quote={quote.quote} 
+                    author={quote.author} 
+                    key={i}
+                />
+            )
+           })}
         </div>
     );
 };
