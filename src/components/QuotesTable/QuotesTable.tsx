2f5029c3018134f9d133a59230a9a46c837225eb
Caru <carolinamaffia.m@gmail.com>
2023-11-01 16:20:57 -0300

    Changed some styles in QuotesTable and modified mediaqueries sizes

--- src/components/QuotesTable/QuotesTable.tsx
+++ src/components/QuotesTable/QuotesTable.tsx
@@ -1,44 +1,31 @@
+import { useContext } from 'react';
 import QuoteCard from '../QuoteCard/QuoteCard';
+import SearchBar from '../SearchBar/SearchBar';
 import './QuotesTable.scss';
+import QuotesContext from '../../Context/quotesContext';
+import { QuotesContextType } from '../../types/QuotesContextType';
 
 const QuotesTable = () => {
-    const quotesArray = [
-        {
-            quote: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit autem saepe",
-            author: 'Lorem ipsum'
-        },
-        {
-            quote: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit autem saepe",
-            author: 'Lorem ipsum'
-        },
-        {
-            quote: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit autem saepe",
-            author: 'Lorem ipsum'
-        },
-        {
-            quote: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit autem saepe",
-            author: 'Lorem ipsum'
-        },
-        {
-            quote: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit autem saepe",
-            author: 'Lorem ipsum'
-        },
-        {
-            quote: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit autem saepe",
-            author: 'Lorem ipsum'
-        },
-    ]
+const { searchedQuotes, handleQuoteSearch } = useContext<QuotesContextType>(QuotesContext);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        handleQuoteSearch(e.target.value);
+    }
+
     return (
         <div className="quotes-table-container">
-           {quotesArray.map((quote, i) => {
-            return (
-                <QuoteCard 
-                    quote={quote.quote} 
-                    author={quote.author} 
-                    key={i}
-                />
-            )
-           })}
+            <SearchBar handleInputChange={handleInputChange} />
+           <div className="quotes">
+            {searchedQuotes.map((quote, i) => {
+                return (
+                    <QuoteCard 
+                        quote={quote.quote} 
+                        author={quote.author} 
+                        key={i}
+                    />
+                )
+            })}
+           </div>
         </div>
     );
 };
