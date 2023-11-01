cc0190d42696ef88408622b0676e6e86389e2f6c
Caru <carolinamaffia.m@gmail.com>
2023-11-01 16:21:47 -0300

    Created QuotesContext and handleQuoteSearch function with mocked array

---
+++ src/Context/quotesContext.tsx
@@ -0,0 +1,63 @@
+import { createContext, useState } from 'react';
+import { QuotesContextType } from '../types/QuotesContextType';
+import { QuoteType } from '../types/QuoteType';
+
+interface QuotesProviderProps {
+    children: JSX.Element,
+}
+
+const quotesArray: QuoteType[] = [
+    {
+        quote: "Test",
+        author: 'Lorem ipsum'
+    },
+    {
+        quote: "TestQuote",
+        author: 'Lorem ipsum'
+    },
+    {
+        quote: "Testtt",
+        author: 'Lorem ipsum'
+    },
+    {
+        quote: "Long test Long test Long test Long test",
+        author: 'Lorem ipsum'
+    },
+    {
+        quote: "Kirby",
+        author: 'Lorem ipsum'
+    },
+    {
+        quote: "Haru",
+        author: 'Lorem ipsum'
+    },
+]
+
+const initialValue: QuotesContextType = {
+    handleQuoteSearch: () => {},
+    searchedQuotes: quotesArray,
+}
+
+const QuotesContext = createContext<QuotesContextType>(initialValue);
+
+export const QuotesProvider = ({ children }: QuotesProviderProps) => {
+    const [quotes, setQuotes] = useState<QuoteType[]>(quotesArray);
+    const [searchedQuotes, setSearchedQuotes] = useState<QuoteType[]>(quotesArray);
+
+    const handleQuoteSearch = (quoteSearch: string) => {
+        const filteredQuotes: QuoteType[] =  quotes.filter((item) => {
+            return item.quote.toLowerCase().includes(quoteSearch.toLowerCase());
+        })
+        setSearchedQuotes(filteredQuotes);
+    }
+
+    const quotesContextData: QuotesContextType = { handleQuoteSearch, searchedQuotes }
+
+    return (
+        <QuotesContext.Provider value={quotesContextData} >
+            {children}
+        </QuotesContext.Provider>
+    );
+};
+
+export default QuotesContext;
