e5703e8d728c3865975fc699605e779588d41075
Caru <carolinamaffia.m@gmail.com>
2023-11-02 12:21:11 -0300

    Implemented add new quote and modal functionality

--- src/components/QuotesTable/QuotesTable.tsx
+++ src/components/QuotesTable/QuotesTable.tsx
@@ -1,20 +1,42 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import QuoteCard from '../QuoteCard/QuoteCard';
 import SearchBar from '../SearchBar/SearchBar';
 import './QuotesTable.scss';
 import QuotesContext from '../../Context/quotesContext';
 import { QuotesContextType } from '../../types/QuotesContextType';
+import Modal from '../Modal/Modal';
+import AddQuotesModalContent from '../AddQuotesModalContent/AddQuotesModalContent';
 
 const QuotesTable = () => {
-const { searchedQuotes, handleQuoteSearch } = useContext<QuotesContextType>(QuotesContext);
+    const { searchedQuotes, handleQuoteSearch, addNewQuote } = useContext<QuotesContextType>(QuotesContext);
+    const [isModalOpen, setModalOpen] = useState(false);
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         handleQuoteSearch(e.target.value);
     }
 
+    const openModal = () => {
+        setModalOpen(true);
+    }
+
+    const closeModal = () => {
+        setModalOpen(false);
+    };
+
+    const handleSubmitCallback = (quote: string, author: string) => {
+        addNewQuote(quote, author);
+        closeModal();
+    };
+
     return (
         <div className="quotes-table-container">
-            <SearchBar handleInputChange={handleInputChange} />
+            <div className="quotes-table-header">
+                <SearchBar handleInputChange={handleInputChange} />
+                <button onClick={openModal} className='add-quote'>Add quote</button>
+            </div>
+            <Modal isOpen={isModalOpen} onClose={closeModal}>
+                <AddQuotesModalContent handleSubmitCallback={handleSubmitCallback}/>
+            </Modal>
            <div className="quotes">
             {searchedQuotes.map((quote, i) => {
                 return (
