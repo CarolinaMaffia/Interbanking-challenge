c43ec64980af7172f327dc332094cc9d07e424a6
Caru <carolinamaffia.m@gmail.com>
2023-11-02 12:15:46 -0300

    Added trashcan icon to quoteCard

--- src/components/QuoteCard/QuoteCard.tsx
+++ src/components/QuoteCard/QuoteCard.tsx
@@ -1,18 +1,22 @@
 import './QuoteCard.scss';
-import Quote from '../../assets/quote.svg';
+import TrashCanIcon from '../../assets/trashcan.svg';
 
 const QuoteCard = ({ quote, author} : { quote: string; author: string }) => {
     return (
         <div className="quotecard-container">
-            <div className='quote-container'>
-                {/* <img 
-                    src={Quote} 
-                    alt="quote icon" 
-                    className='quote-image'
-                /> */}
+            <div className='top-section-container'>
                 <h3 className='quote'>{quote}</h3>
             </div>
+            <div className='bottom-section-container'>
+            <button className="close-button" onClick={() => console.log('test')}>
+                <img 
+                    src={TrashCanIcon} 
+                    alt="An icon to delete the quote" 
+                    className='delete-image'
+                />
+            </button>
             <p className='author'>- {author}</p>
+            </div>
         </div>
     );
 };
